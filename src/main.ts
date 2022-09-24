79432f3c0965470550bbfe84fd6f639bd48a9ec5
Edward Tanguay (windows) <edward@tanguay.info>
2022-09-24 18:24:10 +0200

    showing one page items

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
 import './style.scss';
+import axios from 'axios';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-  <h1>Book Site</h1>
-  <p>Testing</p>
-  </div>
+const rawBooksUrl = 'https://edwardtanguay.netlify.app/share/techBooks.json';
+
+interface IBook {
+	title: string,
+	description: string,
+	language: string
+}
+
+(async () => {
+	const rawBooks: any[] = (await axios.get(rawBooksUrl)).data;
+	const books: IBook[] = [];
+	rawBooks.forEach(rawBook => {
+		const book: IBook = {
+			title: rawBook.title,
+			description: rawBook.description,
+			language: rawBook.language === '' ? 'english' : rawBook.language
+		}
+		books.push(book);
+	});
+	document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+	<div>
+	<h1>Book Site</h1>
+	<p>There are ${books.length} books.</p>
+
+	<div class="books">
+		${books.map(book => {
+			return `
+	<div class="book">
+			<div class="title">${book.title}</div>
+	</div>	
+			`;
+	}).join('')}
+	</div>
+	</div>
 `;
+})();
+
